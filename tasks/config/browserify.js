911ab62d3e0c86be44911bf97e4ca7b46d03ec04
Ethan Miller <ethan@code-cuts.com>
2015-02-08 16:43:24 +0000

    Add new browserify task to sails tasks

---
+++ tasks/config/browserify.js
@@ -0,0 +1,24 @@
+/**
+ * Compile front-end modules, transforms .jsx code to .js on the fly,
+ * and then output all as as bundle to .tmp/public/js/main.js
+ *
+ * // TODO: Setup jQuery and underscore as externals in browserify.
+ *
+ */
+
+module.exports = function(grunt) {
+
+  grunt.config.set('browserify', {
+    dev: {
+      src: 'assets/js/main.jsx',
+      dest: '.tmp/public/js/main.js',
+      options: {
+        debug: true,
+        extensions: ['.jsx'],
+        transform: ['reactify'],
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-browserify');
+};
