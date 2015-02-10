4a65966d574ab98045efdf3e4d64a6d7cfc06a9e
Ethan Miller <ethan@code-cuts.com>
2015-02-10 23:16:40 +0000

    Add empty Comment controller

---
+++ api/controllers/CommentController.js
@@ -0,0 +1,11 @@
+'use strict';
+/**
+ * CommentController
+ *
+ * @description :: Server-side logic for managing books
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+
+};
