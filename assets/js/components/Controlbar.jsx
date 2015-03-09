e48f7e5139dcf89e0e815bc03d1afc5f4f56b4b5
Ethan Zane Miller <ethan@code-cuts.com>
2015-03-09 16:27:33 +0000

    Add functionality to logout button

--- assets/js/components/Controlbar.jsx
+++ assets/js/components/Controlbar.jsx
@@ -1,6 +1,14 @@
+/**
+ * Controlbar
+ */
 (function() {
 	'use strict';
 
+	// Flux.
+	var AuthStore = require('../stores/AuthStore.js');
+	var AuthActions = require('../actions/AuthActions.js');
+
+	// React & Components.
 	var React = require('react');
 	var PrimaryButton = require('./PrimaryButton.jsx');
 	var Router = require('react-router');
@@ -15,36 +23,50 @@
 		},
 
 		componentDidMount: function () {
-			var self = this;
-			$.get('http://private-20206-kanon.apiary-mock.com/authorized', function(data) {
-				self.setState({loggedIn: data.authorized});
-			});
+		 	AuthStore.addLoginListener(this.onLogin);
+		 	AuthStore.addLogoutListener(this.onLogout);
+		},
+
+		componentWillUnmount: function () {
+			AuthStore.removeLoginListener(this.onLogin);
+			AuthStore.removeLoginListener(this.onLogout);
+		},
+
+		logout: function(e) {
+			e.preventDefault();
+			$.ajax({url: '/logout'})
+				.done(function(data) {
+					console.log(data);
+					AuthActions.logout();
+				})
+				.fail(function(jqXhr) {
+					console.log(jqXhr);
+				});
 		},
 
 		render: function() {
-			var rightGroup;
-
-			rightGroup = !this.state.loggedIn ? (
-				<ul className="right-controlbar-group">
-					<li><Link to="login">Login</Link></li>
-				</ul>
-			) : (
-				<ul className="right-controlbar-group">
-					<li><a href="logout">Logout</a></li>
-				</ul>
-			);
-
-			console.log(rightGroup);
-
 			return (
-				<nav id="controlbar">
+				<nav id="controlbar" class="">
 					<div className="controlbar-wrapper">
 						<a href="#" className="brand-logo">Kanon</a>
-						{rightGroup}
+						<ul className="right-controlbar-group">
+							<li><a href="logout" onClick={this.logout}>Logout</a></li>
+						</ul>
 					</div>
 				</nav>
 			);
+		},
+
+		onLogin: function() {
+			console.log('Control::onLogin()');
+			this.setState({loggedIn: true});
+		},
+
+		onLogout: function() {
+			console.log('Control::onLogout()');
+			this.setState({loggedIn: false});
 		}
+
 	});
 
 	module.exports = Controlbar;
