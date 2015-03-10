0e77b8c599857b10da60cef7a75c106302c95c75
Ethan Zane Miller <ethan@code-cuts.com>
2015-03-10 13:01:06 +0000

    Install new version of send invite component

--- assets/js/components/SendInvite.jsx
+++ assets/js/components/SendInvite.jsx
@@ -1,13 +1,11 @@
 /**
- * Send Invite
+ * SendInvite
  */
 (function() {
 	'use strict';
 
 	// React & Components
 	var React = require('react');
-	var Section = require('./Section.jsx');
-	var Input = require('./Input.jsx');
 
 	// Mixins.
 	var Classable = require('./mixins/classable.js');
@@ -22,38 +20,88 @@
 			var wrapClasses = this.getClasses('send-invite form-wrap z-depth-1', {});
 
 			return (
-				<Section className="above-fold">
+				<section className="above-fold">
 					<div className={gridClasses}>
 						<div className={wrapClasses}>
 							<form className="send-invite-form" role="form">
-								<h4>Send an invite...</h4>
-								<Input
-									type="text"
-				          name="name"
-				          ref="name"
-				          placeholder="Full Name" />
-				        <Input
-									type="text"
-				          name="email"
-				          ref="email"
-				          placeholder="Email Address" />
-				        <Input
-									type="text"
-				          name="title"
-				          ref="title"
-				          placeholder="Title" />
-			          <Input
-									type="text"
-				          name="institution"
-				          ref="institution"
-				          placeholder="Institution" />
+								<h3>Send an invite...</h3>
+								<h5>Invite your colleagues to Kanon.</h5>
+								<fieldset>
+									<div className="fieldset-wrap">
+										<legend>To:</legend>
+										<div className="to-fields-wrap">
+											<div>
+											<input
+												type="text"
+							          name="name"
+							          ref="name"
+							          placeholder="Full Name" />
+							        </div>
+							        <div>
+							        <input
+												type="text"
+							          name="email"
+							          ref="email"
+							          placeholder="Email Address" />
+							        </div>
+							        <div>
+							        <input
+												type="text"
+							          name="title"
+							          ref="title"
+							          placeholder="Title" />
+							        </div>
+							        <div>
+						          <input
+												type="text"
+							          name="institution"
+							          ref="institution"
+							          placeholder="Institution" />
+							        </div>
+						        </div>
+						      </div>
+				        </fieldset>
+				        <hr/>
+				        <fieldset>
+				        	<div className="fieldset-wrap">
+				        		<legend>Message:</legend>
+				        		<div className="message-fields-wrap">
+				        			<div>
+				        			<label htmlFor="recipient">Dear</label>
+				        			<input
+												type="text"
+							          name="recipient"
+							          ref="recipient"
+							          placeholder="Name" /><span className="comma">,</span>
+							        </div>
+							        <div>
+							        <textarea 
+							        	name="message"
+							        	ref="message" >
+							        	I would like to invite you to Kanon, a collaborative space for academics to discuss scholarship and ideas.
+							        </textarea>
+							        </div>
+							        <div>
+							        <input
+							        	type="text"
+							        	name="link"
+							        	ref="link"
+							        	placeholder="[Link will appear]"
+							        	disabled />
+							        </div>
+				        		</div>
+				        	</div>
+				        </fieldset>
+				        <input type="submit" name="submit" value="Send" />
 			        </form>
 			      </div>
 			    </div>
-				</Section>
+				</section>
 			);
 		}
 	});
 
 	module.exports = SendInvite;
+
+
 }());
