df75215766f3dd2c2f8fc451341e489c8a2e0157
Ethan Zane Miller <ethan@code-cuts.com>
2015-05-18 17:14:29 +0200

    Clean up RecommendationController

--- api/controllers/RecommendationController.js
+++ api/controllers/RecommendationController.js
@@ -10,53 +10,49 @@
 
 module.exports = {
 
-	create: function(req, res, next) {
-		console.log('RecommendationController::create() called');
+  create: function(req, res, next) {
+    var user = req.param('user'),
+        source = req.param('source'),
+        rating = req.param('rating'),
+        text = req.param('text'),
+        newRec;
 
-		var user = req.param('user'),
-				source = req.param('source'),
-				rating = req.param('rating'),
-				text = req.param('text'),
-				newRec;
+    if ( !user || !source || !rating ) {
+      return res.badRequest('Invalid paramaters supplied when trying to create a new recommendation.');
+    }
 
-		console.log(req.params.all());
+    newRec = {
+      'user': user.id,
+      'discipline': user.discipline,
+      'source': source.id,
+      'rating': rating,
+      'text': text
+    };
 
-		if ( !user || !source || !rating ) {
-			return res.badRequest('Invalid paramaters supplied when trying to create a new recommendation.');
-		}
+    if ( source.websource !== 'kanon_db' ) {
+      
+      Source.create(source).then(function(newSource) {
+        newRec.source = newSource.id;
+        Recommendation.create(newRec).then(function(recommendation) {
+          return res.send(recommendation);
+        }).catch(function(err) { throw err });
+      }).catch(function(reason) {
+        sails.log.error(CustomErrors.createFailedToPersistError('Failed to create a new source when trying to create new recommendation.'));
+        return res.serverError({err: reason});
+      });
 
-		newRec = {
-			'user': user.id,
-			'discipline': user.discipline,
-			'source': source.id,
-			'rating': rating,
-			'text': text
-		};
+    } else {
 
-		if ( source.websource !== 'kanon_db' ) {
-			
-			Source.create(source).then(function(newSource) {
-				newRec.source = newSource.id;
-				Recommendation.create(newRec).then(function(recommendation) {
-					return res.send(recommendation);
-				}).catch(function(err) { throw err });
-			}).catch(function(reason) {
-				sails.log.error(CustomErrors.createFailedToPersistError('Failed to create a new source when trying to create new recommendation.'));
-				return res.serverError({err: reason});
-			});
+      Recommendation.create(newRec).then(function(recommendation) {
+        return res.send(recommendation);
+      }).catch(function(err) { 
+        sails.log.error(CustomErrors.createFailedToPersistError('Failed to create a new source when trying to create new recommendation.'));
+        return res.serverError({err: reason});
+      });
 
-		} else {
+    }
 
-			Recommendation.create(newRec).then(function(recommendation) {
-				return res.send(recommendation);
-			}).catch(function(err) { 
-				sails.log.error(CustomErrors.createFailedToPersistError('Failed to create a new source when trying to create new recommendation.'));
-				return res.serverError({err: reason});
-			});
-
-		}
-
-	}	
-	
+  } 
+  
 };
 
