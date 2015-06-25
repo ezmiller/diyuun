6b85b0441307625b8cb7e5ce6c6136749df05d6e
Ethan Zane Miller <ethan@code-cuts.com>
2015-06-25 14:46:57 +0200

    Update Discussion & Comment controllers

--- api/controllers/DiscussionController.js
+++ api/controllers/DiscussionController.js
@@ -6,6 +6,54 @@
  */
 
 module.exports = {
-	
+
+	findOne: function(req, res, next) {
+
+		console.log('DiscussionController::findOne() ', req.params.all());
+
+		Discussion
+			.find(req.param('id'))
+			.populate('comments')
+			.then(function(found) {
+
+				if (_.isEmpty(found)) {
+					throw CustomErrors.createRecordNotFoundError('Unable to find discussion.');
+				}
+
+				return _.first(found);
+
+			})
+			.then(function(found) {
+		
+				Promise.all(found.comments.map(function(comment) {
+
+					// For each comment get the user info and include in result.
+					return User.find(comment.user).then(function(found) {
+						if (_.isEmpty(found)) {
+							throw CustomErrors.createRecordNotFoundError('Unable to find user while fetching discussion.');
+						}
+						comment.user = _.first(found);
+						return;			
+					}).catch(function(err) { throw err; });
+
+				})).then(function() {
+
+					res.send(found);
+
+				}).catch(function(err) { throw err; });
+
+			}).catch(function(err) {
+
+				sails.log.error(err);
+
+				if (err.name === 'RecordNotFoundError') {
+					res.notFound(err.message);
+				} else {
+					res.serverError(err.message);
+				}
+			})
+
+	}
+
 };
 
