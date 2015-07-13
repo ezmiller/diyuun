b5c4c9dfda627c53bebc0fbbb8c4319cccfe3c4b
Ethan Zane Miller <ethan@code-cuts.com>
2015-07-13 18:05:30 +0200

    Make Source component into a standalone

---
+++ assets/js/components/Source.jsx
@@ -0,0 +1,50 @@
+/**
+ * Source
+ */
+(function() {
+	'use strict';
+
+	var React = require('react');
+
+	var Source = React.createClass({
+
+		getDefaultProps: function () {
+	    return {
+      	source: null  
+	    };
+		},
+
+		handleClick: function(e) {
+			e.preventDefault();
+			publish('/app/transitionTo', ['/source/'+this.props.source.get('id')]);
+		},
+
+		render: function() {
+			var source, authors;
+
+			source = this.props.source;
+
+			authors = !source ? null : source.get('authors').map(function(author,key) {
+				return <span key={key} className="author">{author.firstName} {author.lastName}</span>;
+			});
+
+			return(
+				<article className="source book eight columns" onClick={this.handleClick}>
+					<div className="cover">
+						<img className="image" src={source.get('imageLinks').thumbnail} alt="Book Cover" />
+					</div>
+					<div className="info">
+						<div className="meta">
+							<h3 className="title">{source.get('title')}</h3>
+							<h5 className="subtitle">{source.get('subtitle')}</h5>
+							<span className="authors">by {authors}</span>
+						</div>
+					</div>
+				</article>
+			);
+		}
+
+	});
+
+	module.exports = Source;
+}());
--- assets/js/components/UserDashboard.jsx
+++ assets/js/components/UserDashboard.jsx
@@ -6,49 +6,10 @@
 
 	var $ = require('jquery');
 	var React = require('react');
+	var Source = require('./Source.jsx');
 	var Actions = require('../actions/Actions.js');
 	var SourceStore = require('../stores/SourceStore.js');
 	var BackboneModel = require('backbone').Model;
-
-	var Book = React.createClass({
-
-		getDefaultProps: function () {
-	    return {
-      	source: null  
-	    };
-		},
-
-		handleClick: function(e) {
-			e.preventDefault();
-			publish('/app/transitionTo', ['/source/'+this.props.source.get('id')]);
-		},
-
-		render: function() {
-			var source, authors;
-
-			source = this.props.source;
-
-			authors = !source ? null : source.get('authors').map(function(author,key) {
-				return <span key={key} className="author">{author.firstName} {author.lastName}</span>;
-			});
-
-			return(
-				<article className="source book eight columns" onClick={this.handleClick}>
-					<div className="cover">
-						<img className="image" src={source.get('imageLinks').thumbnail} alt="Book Cover" />
-					</div>
-					<div className="info">
-						<div className="meta">
-							<h3 className="title">{source.get('title')}</h3>
-							<h5 className="subtitle">{source.get('subtitle')}</h5>
-							<span className="authors">by {authors}</span>
-						</div>
-					</div>
-				</article>
-			);
-		}
-
-	});
 
 	var UserFeed = React.createClass({
 
@@ -59,15 +20,15 @@
 		},
 
 		render: function() {
-			var books;
+			var sources;
 
-			books = this.props.sources ? this.props.sources.map(function(item, i) {
-				return <Book key={i} source={item} />;
+			sources = this.props.sources ? this.props.sources.map(function(item, i) {
+				return <Source key={i} source={item} />;
 			}) : null;
 
 			return (
 				<div className="user-feed">
-					{books}
+					{sources}
 				</div>
 			);
 		}
