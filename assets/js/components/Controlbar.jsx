48ac157c956d1b55c64574909fd983d975fbacc5
Ethan Zane Miller <ethan@code-cuts.com>
2015-08-01 15:35:56 -0400

    Fix dropdown action on controlbar

--- assets/js/components/Controlbar.jsx
+++ assets/js/components/Controlbar.jsx
@@ -5,6 +5,7 @@
 	'use strict';
 
 	var $ = require('jquery');
+	var classNames = require('classnames');
 
 	// Flux.
 	var AuthStore = require('../stores/AuthStore.js');
@@ -22,8 +23,24 @@
 			user: React.PropTypes.object.isRequired
 		},
 
+		getInitialState: function () {
+		  return {
+		    'dropMenuVisible': false  
+		  };
+		},
+
+		handleMenuClick: function(e) {
+			this.setState({dropMenuVisible: false});
+		},
+
+		handleDropMenuToggle: function(e) {
+			e.preventDefault();
+			this.setState({dropMenuVisible: !this.state.dropMenuVisible});
+		},
+
 		logout: function(e) {
 			e.preventDefault();
+			this.setState({dropMenuVisible: false});
 			$.ajax({url: '/logout'})
 				.done(function(data) {
 					console.log(data);
@@ -35,25 +52,28 @@
 		},
 
 		render: function() {
+			var isVisible = this.state.dropMenuVisible;;
+
 			return (
 				<nav id="controlbar">
 					<div className="controlbar-wrapper">
 						<a href="#" className="brand-logo">Kanon</a>
 						<ul className="right-controlbar-group">
 							<li class="dropdown">
-								<a className="dropdown-toggle" href="#">
+								<a className="dropdown-toggle" onClick={this.handleDropMenuToggle} >
 									<div className="avatar-wrap">	<img className="avatar" src="" /></div>
 									<span className="user">{this.props.user.refine('firstName').value}</span>
 									<i className="fa fa-chevron-down"></i>
 								</a>
-								<ul className="dropdown-menu">
-									<li key="send-invite"><Link to="send-invite">Send Invite</Link></li>
-									<li key="suggest-source"><Link to="suggest">Suggest a Book</Link></li>
-									<li key="logout"><a href="logout" onClick={this.logout}>Logout</a></li>
+								<ul className={classNames('dropdown-menu', {'visible': isVisible})}>
+									<li className="menu-item" key="send-invite"><Link onClick={this.handleMenuClick} to="send-invite">Send Invite</Link></li>
+									<li className="menu-item" key="suggest-source"><Link onClick={this.handleMenuClick} to="suggest">Suggest a Book</Link></li>
+									<li className="menu-item" key="logout"><a href="logout" onClick={this.logout}>Logout</a></li>
 								</ul>
 							</li>
 						</ul>
 					</div>
+					<div onClick={this.handleDropMenuToggle} className={classNames('offclick-layer', {'visible': isVisible})}></div>
 				</nav>
 			);
 		},
